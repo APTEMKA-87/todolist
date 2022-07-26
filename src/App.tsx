ad3e0f8746c043538895588561069e8ef27a6e51
APTEMKA-87 <1274646a1@gmail.com>
2022-07-26 15:53:39 +0300

    add tasks.mam

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,24 @@ import {ToDoList} from "./ToDoList";
 
 function App() {
 
-    const task1 = [
+    let tasks1 = [
         {id: 1, title: 'HTML', isDone: true},
         {id: 2, title: 'JS', isDone: true},
         {id: 3, title: 'REACT', isDone: false},
+        {id: 4, title: 'CSS', isDone: false},
     ]
 
-    const task2 = [
-        {id: 1, title: 'Hello', isDone: true},
-        {id: 2, title: 'I am happy', isDone: false},
-        {id: 3, title: 'Yo', isDone: false},
-    ]
+    const removeTask = (id: number) => {
+        tasks1 = tasks1.filter(t => t.id != id)
+        console.log(tasks1)
+    }
 
     return (
         <div className="App">
-            <ToDoList title='What to learn' tasks={task1}/>
-            <ToDoList title='Songs' tasks={task2}/>
+            <ToDoList title='What to learn'
+                      tasks={tasks1}
+                      removeTask={removeTask}
+            />
         </div>
     );
 }
