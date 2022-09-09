ea6031dd5a12cf656c3359b2408e2b2282e34a73
APTEMKA-87 <1274646a1@gmail.com>
2022-09-09 16:51:46 +0300

    lesson 10 create store.ts 53:00

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -30,7 +30,15 @@ type ActionsType =
     | ChangeTodolistTitleActionType
     | ChangeTodolistFilterActionType
 
-export const todolistsReducer = (state: Array<TodolistType>, action: ActionsType): Array<TodolistType> => {
+export let todolistId1 = v1()
+export let todolistId2 = v1()
+
+const initialState: Array<TodolistType> = [
+    {id: todolistId1, title: 'What to learn', filter: 'all'},
+    {id: todolistId2, title: 'What to buy', filter: 'all'}
+]
+
+export const todolistsReducer = (state: Array<TodolistType> = initialState, action: ActionsType): Array<TodolistType> => {
     switch (action.type) {
         case 'REMOVE-TODOLIST':
             return state.filter(tl => tl.id != action.id)
@@ -55,7 +63,7 @@ export const todolistsReducer = (state: Array<TodolistType>, action: ActionsType
             return [...state]
         }
         default:
-            throw new Error('I don\'t understand this type')
+            return state
     }
 }
 
